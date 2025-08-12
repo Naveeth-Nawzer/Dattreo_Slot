51db37adf98395617549f540eb65db451061520d
Nikshan <nikshanps07@gmail.com>
2025-08-12 12:13:54 +0530

    backend file created

---
+++ backend/Server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+// const task = require("./routes/taskRoutes");
+const app = express();
+const PORT = 5001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+
+// app.use("/api/task", task);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
