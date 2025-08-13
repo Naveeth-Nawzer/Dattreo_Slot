71875968c329a53591f47de025bd1f1598a6fc54
Vanaiyalini <kyalini2001@gmail.com>
2025-08-13 11:42:31 +0530

    Home, Onboarding pages, background animations are done!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TrackQueue from './BookingComponents/TrackQueue'
+import Home from './Home'
+import OnboardingPage from './OnboardingPage'
+import OnboardingPage2 from './OnboardingPage2'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -12,8 +15,11 @@ function App() {
     <>
     <BrowserRouter> <Routes>    
     <Route path='/' element={<TrackQueue />} />
-    
-    
+    <Route path='/home' element={<Home />} />
+    <Route path='/OnboardingPage' element={<OnboardingPage />} />
+    <Route path='/OnboardingPage2' element={<OnboardingPage2 />} />
+
+
     </Routes> </BrowserRouter>
     </>
   )
---
+++ frontend/src/Components/BrushTealWaves.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Vector_24 from '../assets/Vector_24.png'
+import Vector_25 from '../assets/Vector_25.png'
+import wavebg from '../assets/wavebg.png'
+
+const BrushTealWaves = () => {
+  return (
+    < >
+    <div className='fixed w-full h-screen mx-[-150px] my-[-100px]'>
+        <img 
+        src={wavebg}
+        alt='wavebg'
+        />  
+    </div>
+    </>
+  )
+}
+
+export default BrushTealWaves
